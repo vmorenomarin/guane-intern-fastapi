b22ab6f88fe35d78200a8978aa6816928176196a
vmorenomarin <vmorenomarin@gmail.com>
2021-10-25 21:27:20 -0500

    Firts commit with enviroment dependences and definition of path parameters and querry parameter for get method.

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Path operation definitions with path parameters and query parameters.
+
+@app.get("/")
+async def home():
+    return {"message": "Ok"}
+
+@app.get("/api/dogs")
+async def dogs():
+    return {"message": "list dogs"}
+
+@app.get("/api/dogs/{dog_name}")
+async def show_dog(dog_name: str):
+    return {"dog_name": dog_name}
+
+@app.get("/api/dogs?is_adopted=True")
+async def is_adopted():
+    return {"dog_name": dog_name}
