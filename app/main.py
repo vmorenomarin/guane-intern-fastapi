fafb1d01bb8fc8ce7bac002950f1d043cd53f47d
vmorenomarin <vmorenomarin@gmail.com>
2021-10-26 01:01:43 -0500

    Added examples to model.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 # from typing import Optional
-from pydantic.fields import Field
+from pydantic import Field
 import uuid
 import requests
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi import Body
-
+from fastapi import Body, Path, Query
 app = FastAPI()
 
 # Models
@@ -14,11 +13,22 @@ app = FastAPI()
 
 class Dog(BaseModel):
     id: str = Field(default=uuid.uuid4())
-    name: str
+    name: str = Field(..., min_length=1, max_length=50)
     picture: str = Field(default=requests.get(
         "https://dog.ceo/api/breeds/image/random").json()["message"])
     is_adopted: bool
     create_date: datetime = None
+
+    class Config:
+        schema_extra = {
+            "example":
+            {
+                "name": "Tito",
+                "picture": "https://images.dog.ceo/breeds/mastiff-english/2.jpg",
+                "is_adopted": False,
+                "create_date": "2021, 10, 25, 22, 56, 15, 858589"
+            }
+        }
 
 
 # Path operation definitions with path parameters and query parameters.
@@ -37,15 +47,31 @@ async def dogs():
 
 
 @app.get("/api/dogs/{dog_name}")
-async def show_dog(dog_name: str):
+async def show_dog(dog_name: str = Path(
+        ...,
+        min_length=1,
+        max_length=20,
+        title="Dog Name",)):
     return {"dog_name": dog_name}
 
 
-@app.get("/api/dogs?is_adopted=True")
-async def is_adopted():
+@app.get("/api/dogs")
+async def is_adopted(
+    is_adopted: bool = Query(True)
+):
     return {"dog_name": None}
 
 
 @app.post("/api/dogs/new")
 async def create_dog(dog: Dog = Body(...)):
+    return dog
+
+
+@app.put("/api/dogs/{name}")
+async def update_dog(
+    name: str = Path(
+        ...,
+    ),
+    dog: Dog = Body(...)
+):
     return dog
