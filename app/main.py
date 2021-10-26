62d5c84c1adba9109dedd90ff20d3f72198775f6
vmorenomarin <vmorenomarin@gmail.com>
2021-10-25 21:31:56 -0500

    .gitignore modifications.

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ app = FastAPI()
 
 # Path operation definitions with path parameters and query parameters.
 
+## Request and Response Body
+
 @app.get("/")
 async def home():
     return {"message": "Ok"}
@@ -19,3 +21,8 @@ async def show_dog(dog_name: str):
 @app.get("/api/dogs?is_adopted=True")
 async def is_adopted():
     return {"dog_name": dog_name}
+
+
+@app.post("dog/new")
+async def create_dog():
+    pass
